d880beff701316948159e72baa6c508c66311d8e
trela677 <236677@edu.p.lodz.pl>
2022-04-14 12:53:43 +0200

    Base Gradle Spring Boot project.

---
+++ ticketbookingapp/src/main/java/pl/touk/recruitmenttask/ticketbookingapp/TicketbookingappApplication.java
@@ -0,0 +1,13 @@
+package pl.touk.recruitmenttask.ticketbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbookingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbookingappApplication.class, args);
+	}
+
+}
---
+++ ticketbookingapp/src/test/java/pl/touk/recruitmenttask/ticketbookingapp/TicketbookingappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.touk.recruitmenttask.ticketbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
